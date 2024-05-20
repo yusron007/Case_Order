70c0921a27258edb96ca098d592a336ef2b37edc
yusron007 <rifkiyusran10@gmail.com>
2024-05-20 15:02:50 +0700

    inisialisasi project spring boot and angular

---
+++ Be_order/src/test/java/com/casebtpn/Be_order/BeOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.casebtpn.Be_order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
